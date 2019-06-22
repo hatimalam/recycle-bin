922eb27a9f613ef5be06357330756e3f8cc86e27
hatimalam <hatimalam@gmail.com>
2019-06-22 23:30:57 +0800

    include bean file

---
+++ src/custom/Extension/application/Ext/Include/RecycleBin.php
@@ -0,0 +1,9 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['lab_RecycleBin'] = 'lab_RecycleBin';
+$beanFiles['lab_RecycleBin'] = 'modules/lab_RecycleBin/lab_RecycleBin.php';
+$modules_exempt_from_availability_check['lab_RecycleBin'] = 'lab_RecycleBin';
+$report_include_modules['lab_RecycleBin'] = 'lab_RecycleBin';
+$modInvisList[] = 'lab_RecycleBin';
+
+?>
