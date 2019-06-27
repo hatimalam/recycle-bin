50fae314ce6f520f7809c38fffcac549465ea2d7
hatimalam <hatimalam@gmail.com>
2019-06-28 01:10:42 +0800

    #Fix: Deleting record from Recycle Bin creates the record again, add exception

--- src/custom/include/RB_Hooks/RB_AppHook.php
+++ src/custom/include/RB_Hooks/RB_AppHook.php
@@ -7,10 +7,13 @@ class RB_AppHookClass
     {
         $GLOBALS['log']->fatal("Event is: ".$event);
         $GLOBALS['log']->fatal("Module is: ".$bean->module_dir);
-        $rb_bean = BeanFactory::newBean("hats_Recycle_Bin");
-        $rb_bean->name = "{$bean->name} - deleted";
-        $rb_bean->related_bean = $bean->module_dir;
-        $rb_bean->related_bean_id = $bean->id;
-        $rb_bean->save();
+        if($bean->module_dir !== 'hats_Recycle_Bin') {
+            $rb_bean = BeanFactory::newBean("hats_Recycle_Bin");
+            $rb_bean->name = "{$bean->name} - deleted";
+            $rb_bean->related_bean = $bean->module_dir;
+            $rb_bean->related_bean_id = $bean->id;
+            $rb_bean->assigned_user_id = $GLOBALS['current_user']->id;
+            $rb_bean->save();
+        }
     }
 }
