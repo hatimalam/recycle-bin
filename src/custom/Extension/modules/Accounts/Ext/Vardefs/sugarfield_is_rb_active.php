f91a38913e947b1f192751a0906f9fa4ff28384f
hatimalam <hatimalam@gmail.com>
2019-06-30 23:32:33 +0800

    Field to identify whether module is RB active

---
+++ src/custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_is_rb_active.php
@@ -0,0 +1,8 @@
+<?php
+
+$dictionary['Account']['fields']['is_rb_active']['name'] = 'is_rb_active';
+$dictionary['Account']['fields']['is_rb_active']['vname'] = 'LBL_IS_RB_ACTIVE';
+$dictionary['Account']['fields']['is_rb_active']['type'] = bool;
+$dictionary['Account']['fields']['is_rb_active']['audited'] = true;
+$dictionary['Account']['fields']['is_rb_active']['required'] = false;
+$dictionary['Account']['fields']['is_rb_active']['default'] = 0;
--- src/custom/include/RB_Hooks/RB_AppHook.php
+++ src/custom/include/RB_Hooks/RB_AppHook.php
@@ -1,13 +1,13 @@
 <?php
 if(!defined('sugarEntry') || !sugarEntry) die('Not a valid entry point');
 
-class RB_AppHookClass
+class TBAppHookClass
 {
     public function afterDelete($bean, $event, $arguments)
     {
         $GLOBALS['log']->fatal("Event is: ".$event);
         $GLOBALS['log']->fatal("Module is: ".$bean->module_dir);
-        if($bean->module_dir !== 'hats_Recycle_Bin') {
+        if(isset($bean->is_rb_active) && ($bean->is_rb_active)) {
             $rb_bean = BeanFactory::newBean("hats_Recycle_Bin");
             $rb_bean->name = "{$bean->name} - deleted";
             $rb_bean->related_bean = $bean->module_dir;
