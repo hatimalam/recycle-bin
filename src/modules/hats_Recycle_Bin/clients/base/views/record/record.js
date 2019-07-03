1c1771c8010672b726ae0fa5b89bee047a125576
hatimalam <hatimalam@gmail.com>
2019-07-03 23:24:06 +0800

    Custom delete message

--- src/modules/hats_Recycle_Bin/clients/base/views/record/record.js
+++ src/modules/hats_Recycle_Bin/clients/base/views/record/record.js
@@ -19,4 +19,10 @@
   			},
   		});
   	},
+
+    getDeleteMessages: function() {
+      var messages = this._super('getDeleteMessages');
+      messages.confirmation = "Deleting this record will delete the link to restore data. Are you sure you want to delete it?";//app.utils.formatString(app.lang.get('NTC_DELETE_CONFIRMATION_FORMATTED'), [context]);
+      return messages;
+    },
 })
