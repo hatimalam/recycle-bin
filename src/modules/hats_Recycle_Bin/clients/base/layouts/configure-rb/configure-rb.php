a45ad03d21ea32ffbc2049c34d63c328c93a52ae
hatimalam <hatimalam@gmail.com>
2019-07-16 23:29:15 +0800

    Custom layout for modules configuration

---
+++ src/modules/hats_Recycle_Bin/clients/base/layouts/configure-rb/configure-rb.php
@@ -0,0 +1,10 @@
+<?php
+
+$viewdefs['hats_Recycle_Bin']['base']['layout']['configure-rb'] = array(
+    'type' => 'simple',
+    'components' => array(
+        array(
+            'view' => 'configure-rb-view'
+        ),
+    ),
+);
---
+++ src/modules/hats_Recycle_Bin/clients/base/views/configure-rb-view/configure-rb-view.js
@@ -0,0 +1,7 @@
+({
+  className: 'configure-rb-view',
+  initialize: function(options) {
+    console.log('configure rb view..');
+    this._super('initialize', [options]);
+  },
+})
