c4c6e59d9d215bfe084f2824a4830f73c11bbd5b
hatimalam <hatimalam@gmail.com>
2019-07-17 01:36:50 +0800

    Custom layout update

--- src/modules/hats_Recycle_Bin/clients/base/views/configure-rb-view/configure-rb-view.js
+++ src/modules/hats_Recycle_Bin/clients/base/views/configure-rb-view/configure-rb-view.js
@@ -1,7 +1,27 @@
 ({
   className: 'configure-rb-view',
+  moduleList: [],
   initialize: function(options) {
     console.log('configure rb view..');
     this._super('initialize', [options]);
+    // this.on('render', this._getEnabledModules);
   },
+
+  _getEnabledModules: function () {
+    console.log('called');
+    var self = this;
+    var modules = app.metadata.get().modules_info;
+    _.each(modules, function(module_attr, module_name) {
+      // console.log(module_name);
+      if(module_attr.visible) {
+        self.moduleList.push(module_name);
+      }
+    });
+    console.log(self.moduleList);
+  },
+
+  render: function() {
+    this._getEnabledModules();
+    this._super('render');
+  }
 })
